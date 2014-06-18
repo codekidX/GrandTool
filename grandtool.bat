938f8c385a030e33818b7a1499f55a7188362c46
chotu222 <ckrackerx15@gmail.com>
2014-06-18 20:35:28 +0530

    Add enable dualsim function

--- grandtool.bat
+++ grandtool.bat
@@ -610,6 +610,21 @@ echo --* XDA for the knowledge I gained
 pause
 cls
 goto restart
+:emsim
+cls
+cd chotu
+echo Connect your device
+echo Ensure that adb debugging is enabled in your device
+echo If not enable it and Allow your pc to use adb
+PAUSE
+echo Connecting..
+adb devices
+echo Taking superuser rights from your device ..
+su
+setprop persist.radio.multisim.config dsds
+echo Done
+PAUSE
+goto resrart
 :errjava
 cls
 echo Java was not found, you will not be able to sign apks or use apktool
